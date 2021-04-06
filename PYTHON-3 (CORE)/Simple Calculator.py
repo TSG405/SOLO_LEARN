2039ef4927838a3f9c63c758aed6aac45ba26923
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2021-04-06 12:21:03 +0530

    Create Simple Calculator.py

---
+++ PYTHON-3 (CORE)/Simple Calculator.py
@@ -0,0 +1,20 @@
+'''
+@Coded by TSG, 2021
+
+Problem:
+
+
+Write a program to take two integers as input and output their sum.
+
+Sample Input:
+2
+8
+
+Sample Output:
+10
+'''
+
+
+
+# your code goes here
+print(int(input())+int(input()))
